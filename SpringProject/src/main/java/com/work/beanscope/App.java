b678d8aa3acf2adff2918991feb4567ce9e8260b
vijaykumar <vijay.bandaragalla@GSPLK-CPU0249.girmiti.in>
2023-03-28 17:30:40 +0530

    first commit of Bean scope practiced on Singleton scope

---
+++ SpringProject/src/main/java/com/work/beanscope/App.java
@@ -0,0 +1,28 @@
+package com.work.beanscope;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Project Started" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/work/beanscope/scope.xml");
+        //upcasting
+        
+        SingletonScopeAndAnnotationScope a =(SingletonScopeAndAnnotationScope) context.getBean("student1");
+        
+        System.out.println(a.hashCode()); 
+        
+        SingletonScopeAndAnnotationScope b = (SingletonScopeAndAnnotationScope) context.getBean("student1");
+        System.out.println(b.hashCode()); 
+        //same object has been returened bcz we have sqpecified singletone scope in xml (if we change the scope to prototype we get new object created)
+        		
+        System.out.println("Project Ended");
+    }
+}
