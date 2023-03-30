4292915f9c2fee8276a55fb42aa5c61057d837ac
vijaykumar <vijay.bandaragalla@GSPLK-CPU0249.girmiti.in>
2023-03-30 15:20:15 +0530

    second commit used annotaion @Bean and passed arguments to it

--- SpringProject/src/main/java/com/work/projwithoutxml/MainClient.java
+++ SpringProject/src/main/java/com/work/projwithoutxml/MainClient.java
@@ -5,8 +5,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MainClient {
 	public static void main(String[] args) {
+		System.out.println("execution started");
 		ApplicationContext con = new AnnotationConfigApplicationContext(PizzaConfig.class); 
-		Pizza p=con.getBean("firstPizza",Pizza.class);
+//		Pizza p=con.getBean("firstPizza",Pizza.class);
+//		here we have used bean annotation we have to call the getPizza method 
+//		Pizza p=con.getBean("getPizza",Pizza.class);
+		//here we have passed the name of bean as "a,b,c" we can use any one among it
+		Pizza p=con.getBean("a",Pizza.class);
 		System.out.println(p);
 		p.deliver();
 		System.out.println("execution stopped");
--- SpringProject/src/main/java/com/work/projwithoutxml/PizzaConfig.java
+++ SpringProject/src/main/java/com/work/projwithoutxml/PizzaConfig.java
@@ -1,10 +1,16 @@
 package com.work.projwithoutxml;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.work.projwithoutxml")
+//@ComponentScan(basePackages = "com.work.projwithoutxml")
 public class PizzaConfig {
-	
+//	@Bean()
+	@Bean(name= {"a","b","c"})
+	public Pizza getPizza() {
+		Pizza p = new Pizza();
+		return p;
+	}
 }
